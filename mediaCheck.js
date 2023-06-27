935dcc9fd8a38c790a3b37f22c90546b43c9e572
BenjaminMaring <benjammaring@gmail.com>
2023-06-26 18:33:38 -0700

    final mobile version, just needs hrefs added to links

--- mediaCheck.js
+++ mediaCheck.js
@@ -3,6 +3,9 @@ let buttonSection = document.getElementById("button-section");
 let buttonWrapper = document.getElementById("button-wrapper");
 let menu = document.getElementById("menu");
 let logo = document.getElementById("logo");
+let mobileLinks = document.getElementById("mobile-links");
+
+let footerImg = document.getElementById("footer-img");
 
 let x = window.matchMedia("(max-width: 1100px)");
 mediaQuery(x) // Call listener function at run time
@@ -17,10 +20,26 @@ function mediaQuery(x) {
       buttonWrapper.style = "width: 90vw;";
       logo.style = "width: 100%;";
       menu.style = "position: relative;"
-      
+
+      links.innerHTML = "";
+      links.className = "";
+
+      mobileLinks.innerHTML = "<a href='https://www.mindbodyonline.com/explore/locations/power-up-fitness-studio' class='menu-link'>Classes</a><a href='menu-link' class='menu-link'>Pricing</a><a href='menu-link' class='menu-link'>Contact Us</a>";
+      mobileLinks.classList = "safari-links";
+
+      footerImg.style = "width: 150px;"
     } else {
      review.style = "display: grid;";
      buttonSection.style = "grid-template-columns: repeat(4, 1fr); width: 60vw;"
      buttonWrapper.style = "width: 60vw;";
+
+     links.innerHTML = "<a href='https://www.mindbodyonline.com/explore/locations/power-up-fitness-studio' class='menu-link'>Classes</a><a href='menu-link' class='menu-link'>Pricing</a><a href='menu-link' class='menu-link'>Contact Us</a>";
+     links.classList = "links";
+
+    mobileLinks.innerHTML = "";
+    mobileLinks.classList = "";
+
+    footerImg.style = "width: 250px;"
+
     }
   }
